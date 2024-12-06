2fe16e9e347299a8c6b5f9d742de4e7d2195f6cd
Nicollas Matheus Prado Pinheiro <20241014040014@ifrn.local>
2024-12-06 10:48:14 -0300

    Refatoração aula01 do projeto

--- Projeto/aula1.asm
+++ Projeto/aula1.asm
@@ -3,9 +3,9 @@ main: lui $8, 0x1001 # Endereço inicial
       ori $9, $0, 0xff00 # Salva a cor no reg $9
       
       # Loop de 32 iterações
-      addi $20, $0, 128
+      addi $20, $0, 78
 test: beq $20, $0, fiml
-      addi $10, $0, 97
+      addi $10, $0, 47
       addi $11, $0, 32
       
       slt $10, $20, $10 # testa se $20 está abaixo da iteração 97
